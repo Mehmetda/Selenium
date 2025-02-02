474e0c386fa843060443e7d5f5d99cece57fc664
Mehmetda <mehmetgokmendag@gmail.com>
2025-02-02 22:45:24 +0300

    first commit

---
+++ src/test/java/Lesson_01/First_Class.java
@@ -0,0 +1,18 @@
+package Lesson_01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First_Class {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+        driver.manage().window().maximize();
+        Thread.sleep(200);
+        driver.close();
+    }
+}
